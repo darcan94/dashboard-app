279a8b287be854d629a392cf1c29e18406bef637
Dario Candia <darcanf.94@gmail.com>
2023-12-21 21:11:36 -0300

    Add: authentication with nextAuth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const FormSchema = z.object({
     id: z.string(),
@@ -64,13 +66,22 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
-   
+
+    if (!validatedFields.success) {
+        return {
+          errors: validatedFields.error.flatten().fieldErrors,
+          message: 'Missing Fields. Failed to Update Invoice.',
+        };
+      }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
    
     try {
@@ -92,7 +103,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 
   export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
@@ -100,4 +110,25 @@ export async function updateInvoice(id: string, formData: FormData) {
     } catch (error) {
         return { message: 'Database Error: Failed to Delete Invoice.' };
     }        
+  }
+
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData
+  ){
+    try {
+        await signIn('credentials', formData)
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.stack) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentilas.'
+            
+                default:
+                    return 'Something went wrong.'
+            }
+        }
+        throw error
+    }
   }
